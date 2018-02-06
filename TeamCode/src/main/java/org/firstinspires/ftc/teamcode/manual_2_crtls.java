208f5e14cba91bfef5eae36b320dbfa6b52b317c
Hima Aramona <haramona@gmail.com>
2018-02-05 16:35:07 -0800

    tonyrobot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manual_2_crtls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manual_2_crtls.java
@@ -21,7 +21,6 @@ public class manual_2_crtls extends LinearOpMode {
     private DcMotor rightFront;
 
     private DcMotor liftMotor;
-    private DcMotor ramp;
 
     private DcMotor lBelt;
     private DcMotor rBelt;
@@ -65,7 +64,7 @@ public class manual_2_crtls extends LinearOpMode {
         rBelt = hardwareMap.dcMotor.get("RBELT");
 
         liftMotor = hardwareMap.dcMotor.get("LIFT");
-        ramp = hardwareMap.dcMotor.get("ramp");
+
 
         limtTop = hardwareMap.get(DigitalChannel.class, "touch_top");
         limtBot = hardwareMap.get(DigitalChannel.class, "touch_bot");
@@ -91,15 +90,18 @@ public class manual_2_crtls extends LinearOpMode {
             if(gamepad1.dpad_left){colSer.setPosition(.9); knckSer.setPosition(.5);}
             if(gamepad1.dpad_right){lBelt.setPower(0);rBelt.setPower(0);}
 
-            if(gamepad1.y){ramp.setPower(-0.5);
-            } else if(gamepad1.x){ramp.setPower(0.5);
-            } else{ramp.setPower(0);}
-
-            if(gamepad1.left_bumper){lDum.setPosition(.15);rDum.setPosition(.85);}
-            if(gamepad1.right_bumper){lDum.setPosition(.7);rDum.setPosition(.3);}
 
 
-            aButton();
+            if(gamepad1.a){lDum.setPosition(.15);rDum.setPosition(.85);}
+            if(gamepad1.b){lDum.setPosition(1);rDum.setPosition(0);}
+            if(gamepad1.x){lDum.setPosition(.45);rDum.setPosition(.45);}
+            if(gamepad1.y){lDum.setPosition(0);rDum.setPosition(1);}
+
+
+
+
+
+
 
             //mecaum
             leftFront.setPower(curveSqr(((gamepad2.left_trigger - gamepad2.right_trigger)*-0.5)-gamepad1.left_stick_y + gamepad1.left_stick_x+ gamepad1.right_stick_x - gamepad2.left_stick_y + gamepad2.left_stick_x+ gamepad2.right_stick_x )*.5
@@ -160,31 +162,7 @@ public class manual_2_crtls extends LinearOpMode {
         }
     }
 
-    private void setPos(long in, long out, double alignInput, long sTime ){
-        if((System.currentTimeMillis() >= (in+sTime)) && (System.currentTimeMillis() <= (out+sTime))){
-            align.setPosition(alignInput);
-        }
-    }
-
-    private void aButton(){
-        if (gamepad1.a){
-            startTime = System.currentTimeMillis();
-            align.setPosition(0);
-            lBelt.setPower(0);
-            rBelt.setPower(0);
-        }
-
-        setPos(100,300,.5,startTime);
-        setPos(300,500,.6,startTime);
-        setPos(500,700,.45,startTime);
-        setPos(900,1500,.8,startTime);
 
 
-        if((System.currentTimeMillis() > (startTime+1500)) && (System.currentTimeMillis() < (startTime+2000))) {
-            align.setPosition(0);
-//            lBelt.setPower(1);
-//            rBelt.setPower(-1);
-        }
-    }
 
 }
