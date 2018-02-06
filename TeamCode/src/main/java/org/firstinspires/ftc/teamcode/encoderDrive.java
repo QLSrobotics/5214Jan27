f62d3368f39116e7df34c7aa6cb03918992ba72a
William Ng <williamwu2049@outlook.com>
2018-02-06 15:52:49 -0800

    added encoder motor drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderDrive.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@Autonomous(name="jan18_testing", group="Team5214")
+//@Disabled
+public class encoderDrive extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    //declare drive motors
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    final double diameter = 0.05; //unit in meters, fuck inches
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //mapping drive motors to configuration
+        leftBack  = hardwareMap.get(DcMotor.class, "LB");
+        rightBack = hardwareMap.get(DcMotor.class, "RB");
+        leftFront  = hardwareMap.get(DcMotor.class, "LF");
+        rightFront = hardwareMap.get(DcMotor.class, "RF");
+
+        //drive motor directions
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            //sample, drive 5 meters in 0.25 power
+            encoderDrive(leftBack, 0.25, 5);
+
+            telemetry.update();
+            //break;
+        }
+    }
+    private void encoderDrive(DcMotor motor, double power, double distance) {
+        //distance unit in meters, fuck inches
+        double currentRevolution = motor.getCurrentPosition();
+        double revolutions = distance / (diameter*Math.PI);
+        double targetRevolution = (currentRevolution + revolutions);
+
+        //casted double value to an integer
+        motor.setTargetPosition((int)targetRevolution);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setPower(power);
+        while(motor.isBusy()){
+            //do nothing
+        }
+    }
+}
